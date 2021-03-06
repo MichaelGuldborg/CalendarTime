681703e57a47382c2969c307e2b47251951d32f5
Michael Guldborg <mail@michaelguldborg.dk>
2021-03-06 19:11:24 +0100

    Clean config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,10 +6,6 @@ const isProductionMode = API_ENV === 'production';
 const isDevelopmentMode = API_ENV === 'development' || API_ENV === 'staging';
 const isLocalhostMode = API_ENV === 'localhost';
 
-const googleClientId = '419934641506-16mqcque3vpo5gcath9vsupqvq02a7ug.apps.googleusercontent.com';
-const googleClientSecret = 'bdoIGHNODMTeLlV4X-6FvS0m';
-const googleCalendarAPIKey = 'AIzaSyA38EM1jX0DMDaGu8Xq_GgX04hAbcq9ezU';
-
 export const config = {
     API_ENV,
     isLocalhostMode,
@@ -17,6 +13,5 @@ export const config = {
     isProductionMode,
     name: packageJson['name'],
     version: packageJson['version'],
-    googleClientId,
 }
 export default config;
