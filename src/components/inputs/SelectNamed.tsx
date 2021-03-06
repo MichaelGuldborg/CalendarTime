920389ac86bd91706da41f09efae47b0288aa6a7
Michael Guldborg <mail@michaelguldborg.dk>
2021-03-06 19:31:06 +0100

    Fix lint

--- src/components/inputs/SelectNamed.tsx
+++ src/components/inputs/SelectNamed.tsx
@@ -44,6 +44,7 @@ export const SelectNamed: React.FC<SelectNamedProps> =
                 return (selected as unknown[])?.map(u => {
                     if (typeof u === 'string') return idValueMap[u]?.name;
                     if (typeof u === 'object') return idValueMap[(u as Identifiable).id]?.name
+                    return '' + u;
                 })?.join(', ');
             }
             if (typeof selected === 'string') {
