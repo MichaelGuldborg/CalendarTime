a898b4219764bcfd70316f1e1ba0a653d02b4bd4
Michael Guldborg <mail@michaelguldborg.dk>
2021-03-22 11:32:40 +0100

    Add gtag

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactGA from 'react-ga';
 
-ReactGA.initialize('G-Y1B35DCG29');
 
 ReactDOM.render(
     <React.StrictMode>
